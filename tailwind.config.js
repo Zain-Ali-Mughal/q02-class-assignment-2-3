777e10778e61f1e314da74b0116b769ffba6d176
Zain Ali <164686396+Zain-Ali-Mughal@users.noreply.github.com>
2024-10-01 17:10:38 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS/TS files in the src folder
+    "./pages/**/*.{js,ts,jsx,tsx}", // If you have a pages directory
+    "./components/**/*.{js,ts,jsx,tsx}", // Include your components
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
